1a5d4de68310ff91480a74fc073116c9a5d15185
Jovi <jcsbmwxyha@gmail.com>
2018-06-16 23:22:41 +0800

    NotifyChangeExample first commit

---
+++ Ch0904_NotifyChangeExample/Ch0904/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+namespace Ch0904
+{
+    public sealed partial class MainPage : Page
+    {
+        MyColors textcolor = new MyColors();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // 設定筆刷顏色Brush1為紅色
+            tb.Text = "文字顏色設為紅色 !";
+            textcolor.Brush1 = new SolidColorBrush(Colors.Red);
+            tb.DataContext = textcolor;
+            ShowMess("文字顏色設為紅色 !");
+        }
+        async private void ShowMess(string res)
+        {
+            var messDialog = new MessageDialog(res);
+            await messDialog.ShowAsync();
+        }
+        private void btCol_Click(object sender, RoutedEventArgs e)
+        {  // 改變筆刷顏色Brush1為藍色
+            tb.Text = "文字顏色改變為藍色 !";
+            textcolor.Brush1 = new SolidColorBrush(Colors.Blue);
+            tb.DataContext = textcolor;
+        }
+    }
+}
---
+++ Ch0904_NotifyChangeExample/Ch0904/MyColors.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Media;
+
+namespace Ch0904
+{
+    public class MyColors : INotifyPropertyChanged
+    {
+        private SolidColorBrush _Brush1;
+
+        // 宣告PropertyChanged事件
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // 建立綁定的資料來源(為一筆刷顏色)
+        public SolidColorBrush Brush1
+        {
+            get { return _Brush1; }
+            set
+            {
+                _Brush1 = value;
+                // 當來源屬性(即筆刷顏色)有改變時呼叫NotifyPropertyChanged
+                NotifyPropertyChanged("Brush1");
+            }
+        }
+
+        // NotifyPropertyChanged 方法將導致PropertyChanged事件，
+        // 指示來源屬性有改變了
+        async public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this,
+                    new PropertyChangedEventArgs(propertyName));
+                string res = "文字顏色已經改變為藍色 !";
+                var messDialog = new MessageDialog(res);
+                await messDialog.ShowAsync();
+            }
+        }
+    }
+}
