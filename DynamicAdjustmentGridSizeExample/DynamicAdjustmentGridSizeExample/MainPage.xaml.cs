aab046405c1d72ba0a33dd9bd5c531f54be4abef
Jovi <jcsbmwxyha@gmail.com>
2018-06-17 00:21:34 +0800

    DynamicAdjustmentGridSizeExample first commit

---
+++ DynamicAdjustmentGridSizeExample/DynamicAdjustmentGridSizeExample/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DynamicAdjustmentGridSizeExample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public double MainGridHeight;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MainGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainGridHeight = GridRow0.ActualHeight + GridRow1.ActualHeight;
+        }
+
+        private void MainGrid_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            FrameworkElement fe = sender as FrameworkElement;
+            Point p = e.GetCurrentPoint(fe).Position;
+            PointerPoint ptrPt = e.GetCurrentPoint(fe);
+            
+            if (p.Y < GridRow0.Height + 10 && p.Y > GridRow0.Height - 10 && ptrPt.Properties.IsLeftButtonPressed)
+            {
+                GridRow0.Height = p.Y;
+                GridRow1.Height = MainGridHeight - GridRow0.Height;
+            }
+        }
+    }
+}
