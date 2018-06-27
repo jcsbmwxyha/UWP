7e043a06feb71fa5fd98ee32ee4b81f283fcfb4f
Jovi <jcsbmwxyha@gmail.com>
2018-06-27 10:03:03 +0800

    VocabularyTest first commit

---
+++ VocabularyTest/VocabularyTest/VocabularyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace VocabularyTest
+{
+    public class Vocabulary
+    {
+        public Vocabulary(string e, string c, string t)
+        {
+            English = e;
+            Chinese = c;
+            Translater = t;
+            IsSelect = Visibility.Collapsed;
+        }
+        public string English { get; set; }
+        public string Chinese { get; set; }
+        public string Translater { get; set; }
+        public Windows.UI.Xaml.Visibility IsSelect { get; set; }
+    }
+}
