1f5a858e10f05c504789d5c2f8da840ae8c262e7
Jovi <jcsbmwxyha@gmail.com>
2018-07-16 02:10:57 +0800

    [MVVMTest] first commit

---
+++ MVVMTest/MVVMTest/MyViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MVVMTest
+{
+    public class MyViewModel : INotifyPropertyChanged
+    {
+        private RelayCommand _sendEffectCommand;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand UpdateTitleName
+        {
+            get
+            {
+                return (_sendEffectCommand ?? (_sendEffectCommand = new RelayCommand(UpdateTextExecute, CanUpdateTextExecute)));
+            }
+        }
+
+        public MyModel Model { get; set; }
+        public string TheText
+        {
+            get {
+                return Model.MyText;
+            }
+            set
+            {
+                if (Model.MyText != value)
+                {
+                    Model.MyText = value;
+                    RaisePropertyChanged("TheText");
+                    _sendEffectCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        public MyViewModel()
+        {
+            Model = new MyModel { MyText = "" };
+        }
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        void UpdateTextExecute()
+        {
+            TheText = "SkyMVVM";
+        }
+
+        //定義是否可以更新Title
+        bool CanUpdateTextExecute()
+        {
+            return true;
+        }
+    }
+}
---
+++ MVVMTest/MVVMTest/RelayCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MVVMTest
+{
+    public class RelayCommand : ICommand
+    {
+        readonly Action _execute;
+        readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+        
+        public Boolean CanExecute(Object parameter)
+        {
+            return _canExecute == null ? true : _canExecute();
+        }
+
+        public void Execute(Object parameter)
+        {
+            _execute();
+        }
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+    }
+}
