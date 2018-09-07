1ba5a7abfb5cb4be363c746e3920a7ff82117060
minchih_lai <MinChih_Lai@asus.com>
2018-09-07 14:52:07 +0800

    [ListViewItemStyle] 0907

---
+++ ListViewItemStyle/ListViewItemStyle/Item.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 使用者控制項項目範本記載於 https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace ListViewItemStyle
+{
+    public sealed partial class Item : UserControl
+    {
+        public Item()
+        {
+            this.InitializeComponent();
+        }
+        
+        private void Grid_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "PointerOver", false);
+        }
+
+        private void Grid_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Pressed", false);
+        }
+
+        private void Grid_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Normal", false);
+        }
+    }
+}
