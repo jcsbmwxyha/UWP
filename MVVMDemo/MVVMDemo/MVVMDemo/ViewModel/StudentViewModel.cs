d87029235a7348fd94d6bc414c24b3a8f100a3ab
Jovi <jcsbmwxyha@gmail.com>
2018-11-23 15:02:23 +0800

    [MVVMDemo] View & ViewModel Communication - Command

--- MVVMDemo/MVVMDemo/MVVMDemo/ViewModel/StudentViewModel.cs
+++ MVVMDemo/MVVMDemo/MVVMDemo/ViewModel/StudentViewModel.cs
@@ -6,9 +6,12 @@ namespace MVVMDemo.ViewModel
 
     public class StudentViewModel
     {
+        public MyICommand DeleteCommand { get; set; }
+
         public StudentViewModel()
         {
             LoadStudents();
+            DeleteCommand = new MyICommand(OnDelete, CanDelete);
         }
 
         public ObservableCollection<Student> Students
@@ -26,6 +29,32 @@ namespace MVVMDemo.ViewModel
             students.Add(new Student { FirstName = "Linda", LastName = "Hamerski" });
 
             Students = students;
+        }
+
+        private Student _selectedStudent;
+
+        public Student SelectedStudent
+        {
+            get
+            {
+                return _selectedStudent;
+            }
+
+            set
+            {
+                _selectedStudent = value;
+                DeleteCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private void OnDelete()
+        {
+            Students.Remove(SelectedStudent);
+        }
+
+        private bool CanDelete()
+        {
+            return SelectedStudent != null;
         }
     }
 }
